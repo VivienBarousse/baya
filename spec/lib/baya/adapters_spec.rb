9a145d853cc12e7c2f9932b3f0cf1918acb3ad6e
Vivien Barousse <barousse.vivien@gmail.com>
2013-01-01 17:47:19 +0100

    Improve test coverage for Baya::Adapters

---
+++ spec/lib/baya/adapters_spec.rb
@@ -0,0 +1,10 @@
+require ROOT + "/baya/adapters"
+
+describe Baya::Adapters do
+  describe "#from_name" do
+    it "should know about the main adapters" do
+      described_class.from_name('git').should == Baya::Adapters::Git
+      described_class.from_name('github').should == Baya::Adapters::Github
+    end
+  end
+end
