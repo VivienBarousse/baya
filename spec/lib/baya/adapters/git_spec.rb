5e1bf2d9d3971dd0e3669fff93ebd75d4d307ed9
Vivien Barousse <barousse.vivien@gmail.com>
2013-01-01 17:28:36 +0100

    Improve coverage of Baya::Adapters::Git

--- spec/lib/baya/adapters/git_spec.rb
+++ spec/lib/baya/adapters/git_spec.rb
@@ -54,12 +54,44 @@ describe Baya::Adapters::Git do
   end
 
   describe "#archive" do
-    it "should git-clone in the right folder" do
-      ::Git.should_receive(:clone).with(
-        "ssh://git.repo.that/doesnt/exist/",
-        "/my/imaginary/folder/my/destination"
-      )
-      subject.archive("/my/imaginary/folder")
+    context "when the folder doesn't already exist" do
+      it "should git-clone in the right folder" do
+        ::Git.should_receive(:clone).with(
+          "ssh://git.repo.that/doesnt/exist/",
+          "/my/imaginary/folder/my/destination"
+        )
+        subject.archive("/my/imaginary/folder")
+      end
+    end
+
+    context "when the repository has already been cloned" do
+      let(:git) do
+        mock(:git, :pull => nil)
+      end
+
+      before do
+        ::Git.stub(:open).and_return(git)
+        ::File.stub(:directory?).and_return(true)
+      end
+
+      it "should git-pull the existing repository" do
+        ::Git.should_receive(:open).with("/my/imaginary/folder/my/destination")
+        git.should_receive(:pull)
+        subject.archive("/my/imaginary/folder")
+      end
+    end
+
+    context "when the folder already exists but isn't a Git repository" do
+      before do
+        ::Git.stub(:open).and_raise(ArgumentError.new(""))
+        ::File.stub(:directory?).and_return(true)
+      end
+
+      it "should raise an exception" do
+        proc {
+          subject.archive("/my/imaginary/folder")
+        }.should raise_exception
+      end
     end
   end
 end
