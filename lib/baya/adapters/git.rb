504ba0a8331847ed6c65525f39d6f4a97a70e9a4
Vivien Barousse <barousse.vivien@gmail.com>
2013-06-17 23:12:31 +0200

    Migrate Git provider to default to bare repos
    Also add an option to the git provider to specify if you want
    a bare repo or a full repo

--- lib/baya/adapters/git.rb
+++ lib/baya/adapters/git.rb
@@ -13,17 +13,30 @@ module Baya
         destination = root + '/' + @config['destination']
         if File.directory?(destination)
           begin
-            g = ::Git.open(destination)
-            g.pull
-          rescue ArgumentError => e
-            raise "Folder exists and is not a Git repository: #{destination}"
+            g = ::Git.bare(destination)
+            g.fetch
+          rescue ::Git::GitExecuteError => e
+            begin
+              g = ::Git.open(destination)
+              g.pull
+            rescue ArgumentError => e
+              raise "Folder exists and is not a Git repository: #{destination}"
+            end
           end
         else
-          ::Git.clone(@config['origin'], destination)
+          ::Git.clone(@config['origin'], destination, :bare => bare?)
         end
       end
 
       private
+
+      def bare?
+        if @config['bare'].nil?
+          true
+        else
+          !!@config['bare']
+        end
+      end
 
       def check_config
         raise "`origin` is mandatory" unless @config['origin']
--- spec/lib/baya/adapters/git_spec.rb
+++ spec/lib/baya/adapters/git_spec.rb
@@ -58,31 +58,84 @@ describe Baya::Adapters::Git do
       it "should git-clone in the right folder" do
         ::Git.should_receive(:clone).with(
           "ssh://git.repo.that/doesnt/exist/",
-          "/my/imaginary/folder/my/destination"
+          "/my/imaginary/folder/my/destination",
+          :bare => true
         )
         subject.archive("/my/imaginary/folder")
+      end
+
+      context "when the bare option is given" do
+        context "and it is true" do
+          before do
+            config['bare'] = true
+          end
+
+          it "should pass the option as true" do
+            ::Git.should_receive(:clone).with(
+              "ssh://git.repo.that/doesnt/exist/",
+              "/my/imaginary/folder/my/destination",
+              :bare => true
+            )
+            subject.archive("/my/imaginary/folder")
+          end
+        end
+
+        context "and it is false" do
+          before do
+            config['bare'] = false
+          end
+
+          it "should pass the option as false" do
+            ::Git.should_receive(:clone).with(
+              "ssh://git.repo.that/doesnt/exist/",
+              "/my/imaginary/folder/my/destination",
+              :bare => false
+            )
+            subject.archive("/my/imaginary/folder")
+          end
+        end
       end
     end
 
     context "when the repository has already been cloned" do
       let(:git) do
-        mock(:git, :pull => nil)
+        mock(:git, :fetch => nil, :pull => nil)
       end
 
       before do
+        ::Git.stub(:bare).and_return(git)
         ::Git.stub(:open).and_return(git)
         ::File.stub(:directory?).and_return(true)
       end
 
-      it "should git-pull the existing repository" do
-        ::Git.should_receive(:open).with("/my/imaginary/folder/my/destination")
-        git.should_receive(:pull)
+      it "should git-fetch the existing repository" do
+        ::Git.should_receive(:bare).with("/my/imaginary/folder/my/destination")
+        git.should_receive(:fetch)
         subject.archive("/my/imaginary/folder")
+      end
+
+      context "but it's not a bare repository" do
+        before do
+          git.stub(:fetch).and_raise(::Git::GitExecuteError.new)
+        end
+
+        it "should fallback on the open/pull strategy" do
+          ::Git.should_receive(:open).with("/my/imaginary/folder/my/destination")
+          git.should_receive(:pull)
+          subject.archive("/my/imaginary/folder")
+        end
       end
     end
 
     context "when the folder already exists but isn't a Git repository" do
+      let(:bare_git) do
+        b = mock(:bare_git)
+        b.stub(:fetch).and_raise(::Git::GitExecuteError.new)
+        b
+      end
+
       before do
+        ::Git.stub(:bare).and_return(bare_git)
         ::Git.stub(:open).and_raise(ArgumentError.new(""))
         ::File.stub(:directory?).and_return(true)
       end
